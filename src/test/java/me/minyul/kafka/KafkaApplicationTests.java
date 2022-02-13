01e47f4a11a972a976188bd9e7d29a60ac7e3b3d
minyul <ggomjae@gmail.com>
2022-02-13 12:50:32 +0900

    init

---
+++ src/test/java/me/minyul/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minyul.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
